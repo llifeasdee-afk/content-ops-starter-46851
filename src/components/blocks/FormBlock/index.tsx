acfa6ab075b69009e29df9cb3631988c66b402fe
Demourion Tezeno <demouriontezeno@Demourions-MacBook-Air.local>
2025-09-16 22:01:38 -0500

    Updated form to use Formspree

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -22,51 +22,25 @@ export default function FormBlock(props) {
     }
 
     return (
-        <form
-            className={classNames(
-                'sb-component',
-                'sb-component-block',
-                'sb-component-form-block',
-                className,
-                styles?.self?.margin ? mapStyles({ margin: styles?.self?.margin }) : undefined,
-                styles?.self?.padding ? mapStyles({ padding: styles?.self?.padding }) : undefined,
-                styles?.self?.borderWidth && styles?.self?.borderWidth !== 0 && styles?.self?.borderStyle !== 'none'
-                    ? mapStyles({
-                          borderWidth: styles?.self?.borderWidth,
-                          borderStyle: styles?.self?.borderStyle,
-                          borderColor: styles?.self?.borderColor ?? 'border-primary'
-                      })
-                    : undefined,
-                styles?.self?.borderRadius ? mapStyles({ borderRadius: styles?.self?.borderRadius }) : undefined
-            )}
-            name={elementId}
-            id={elementId}
-            onSubmit={handleSubmit}
-            ref={formRef}
-            data-sb-field-path= {fieldPath}
-        >
-            <div
-                className={classNames('w-full', 'flex', 'flex-wrap', 'gap-8', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}
-                {...(fieldPath && { 'data-sb-field-path': '.fields' })}
-            >
-                <input type="hidden" name="form-name" value={elementId} />
-                {fields.map((field, index) => {
-                    const modelName = field.__metadata.modelName;
-                    if (!modelName) {
-                        throw new Error(`form field does not have the 'modelName' property`);
-                    }
-                    const FormControl = getComponent(modelName);
-                    if (!FormControl) {
-                        throw new Error(`no component matching the form field model name: ${modelName}`);
-                    }
-                    return <FormControl key={index} {...field} {...(fieldPath && { 'data-sb-field-path': `.${index}` })} />;
-                })}
-            </div>
-            {submitButton && (
-                <div className={classNames('mt-8', 'flex', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}>
-                    <SubmitButtonFormControl {...submitButton} {...(fieldPath && { 'data-sb-field-path': '.submitButton' })} />
-                </div>
-            )}
-        </form>
+       <form action="https://formspree.io/f/xpwjeyeb" 
+  method="POST"
+  className="sb-component sb-component-block sb-component-form-block"
+>
+  <input 
+    type="email" 
+    name="email" 
+    placeholder="Your email" 
+    required 
+  />
+
+  <textarea 
+    name="message" 
+    placeholder="Your message" 
+    required
+  ></textarea>
+
+  <button type="submit">Send</button>
+</form>
+
     );
 }
